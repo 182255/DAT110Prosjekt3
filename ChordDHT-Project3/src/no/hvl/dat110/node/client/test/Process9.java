0622c7a376d0c8d9a2357e870cd62ed6d73a55be
Enah <182255@stud.hvl.no>
2019-04-02 12:13:03 +0200

    to 600000

--- ChordDHT-Project3/src/no/hvl/dat110/node/client/test/Process9.java
+++ ChordDHT-Project3/src/no/hvl/dat110/node/client/test/Process9.java
@@ -1,12 +1,11 @@
 package no.hvl.dat110.node.client.test;
 
-
 import no.hvl.dat110.rpc.ChordNodeContainer;
 
 public class Process9 {
 
 	public static void main(String[] args) throws Exception {
-		new ChordNodeContainer("process9", 70000, true);
+		new ChordNodeContainer("process9", 600000, true);
 
 	}
 
